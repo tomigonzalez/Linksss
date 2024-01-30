43a4cd527ba97f9cdc38acf38876a733f25d6962
Tomas Gonzalez <tomimateo2008@hotmail.com>
2024-01-30 20:10:16 -0300

    asd

--- src/components/redirect/Redirect.tsx
+++ src/components/redirect/Redirect.tsx
@@ -6,31 +6,31 @@ const Redirect = () => {
       <ButtonRedirect
         urls="https://portafoliov3-tan.vercel.app/"
         str="Portfolio"
-        img="../public/portafolio.png"
+        img="portafolio.png"
         letter="'Italianno', cursive"
       />
       <ButtonRedirect
         urls="https://linkedin.com/in/tomigonzalez"
         str="Linked-in"
-        img="../public/linkedin.png"
+        img="linkedin.png"
         letter="'League Gothic', sans-serif"
       />
       <ButtonRedirect
         urls="https://github.com/tomigonzalez"
         str="GitHub"
-        img="../public/github.png"
+        img="github.png"
         letter="'League Spartan', sans-serif"
       />
       <ButtonRedirect
         urls="https://www.figma.com/@tomigonnzalez"
         str="Figma"
-        img="../public/Figma.png"
+        img="Figma.png"
         letter="'Raleway', sans-serif"
       />
       <ButtonRedirect
         urls="https://drive.google.com/file/d/1z9XOUSA5HZwYBc702R7rGU2d3-1VFv2s/view?usp=sharing"
         str="Cv"
-        img="../public/cv.png"
+        img="cv.png"
         letter="'Leckerli One', cursive"
       />
     </article>
